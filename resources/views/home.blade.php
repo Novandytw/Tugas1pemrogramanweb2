b9ac39c2ea11f98ec97c23bbf14de8d989065f7d
Novandytw <nopankillua@gmail.com>
2025-09-13 23:19:33 +0700

    Add HomeController and landing page (home.blade.php)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang</title>
+</head>
+<body>
+    <h1>Selamat Datang di Landing Page Laravel!</h1>
+    <p>Ini adalah halaman Home yang dibuat dengan Controller dan View.</p>
+</body>
+</html>
